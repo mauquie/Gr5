c1b0d34172b1164212d85b9dd122a20c5ba65cad
Mamie Ginette <71831166+allanplc2@users.noreply.github.com>
2020-10-06 13:29:45 +0200

    Add files via upload

---
+++ PokerAllan.py
@@ -0,0 +1,40 @@
+couleur=["Careau", "Coeur", "Trèfle", "Pique"]
+rang=["2","3","4","5","6","7","8","9","10","V","D","R","A"]
+
+
+C=[]
+t=1
+for i in range(couleur):
+    for j in rang (rang):
+        C[t]=carte(i,j)
+    t=t+1
+
+
+class Carte():
+    
+    def __init__(self, couleur, rang):
+        self.couleur=couleur
+        self.rang=rang
+        
+
+class Joueur():
+    
+    def __init__(self, nom):
+        self.nom=nom
+        self.tapis=1000
+        self.combinaison
+        self.main
+        
+    def recevoir_carte():
+        
+    def nouvelle_donne():
+        
+    def evaluer():
+        
+class Croupier():
+    
+    
+        
+        
+        
+
