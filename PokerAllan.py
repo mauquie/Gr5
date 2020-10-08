535ea6a8971ce1cd6bf50ee1c12a676c23ba2983
Tante Bertrude <71831295+Kaddmo@users.noreply.github.com>
2020-10-08 15:48:14 +0200

    Update PokerAllan.py

--- PokerAllan.py
+++ PokerAllan.py
@@ -1,4 +1,4 @@
-couleur=["Careau", "Coeur", "Tr�fle", "Pique"]
+couleur=["Careau", "Coeur", "Trèfle", "Pique"]
 rang=["2","3","4","5","6","7","8","9","10","V","D","R","A"]
 
 
@@ -34,7 +34,63 @@ class Joueur():
 class Croupier():
     
     
-        
+----------------------------------------------------
+
+Programme de Morgan
+
+from math import *
+
+0.1222222222couleur=["Careau", "Coeur", "Trèfle", "Pique"]
+rang=["2","3","4","5","6","7","8","9","10","V","D","R","A"]
+
+C=[]
+t=1
+
+for i in range(couleur):
+    for j in rang (rang):
+        C[t]=carte(i,j)
+    t=t+1
+
+class Carte():
+    def __init__(self, couleur, rang):
+        self.couleur=couleur
+        self.rang=rang
+
+class Joueur():
+    def __init__(self, nom):
+        self.nom=nom
+        self.tapis=1000
+        self.combinaison
+        self.main
+
+    def recevoir_carte():
+      pass       
+
+    def nouvelle_donne():
+      pass        
+
+    def evaluer():
+      pass
+
+class Partie:
+   def __init__(self,joueurs):
+     self.joueurs=joueurs
+
+class Coup(Partie):
+  def __init__(self,joueurs):
+    Partie.__init__(self,joueurs)
+    self.croupier
+    self.gagnant
+    self.joueurs=[]
+
+  def abattre():
+    pass
+
+  def distribuer():
+    pass
+
+class Croupier():
+  pass
         
         
 
